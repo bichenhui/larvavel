91447d56d300f61c1e931be022db6f7a1420e260
bichenhui <1295727646@qq.com>
2018-11-13 15:15:47 +0800

    'a'

---
+++ app/Http/Controllers/Edu/Articlecontroller.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\Edu;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class Articlecontroller extends Controller
+{
+	public function index(){
+		echo 12;
+	}
+}
